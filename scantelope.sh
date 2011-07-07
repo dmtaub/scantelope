6348a9fd2deb71fde9273e25be779df733d0ffce
Daniel Taub <daniel.taub@ginkgobioworks.com>
2011-07-07 17:40:32 -0400

    removed optified-specific files

--- scantelope.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-export LD_LIBRARY_PATH=/opt/scantelope/lib/
-export PYTHONPATH="/opt/scantelope/lib/python2.6/dist-packages:/opt/scantelope/lib/python2.6/site-packages"
-start-stop-daemon -b --start --chdir /opt/scantelope --exec /opt/scantelope/serv.py 
